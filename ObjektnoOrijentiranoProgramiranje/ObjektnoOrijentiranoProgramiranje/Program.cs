eadefa6b98bb8bf095efb2e7c455e2a954f0313c
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-10-18 10:09:42 +0300

    Add files via upload

---
+++ ObjektnoOrijentiranoProgramiranje/ObjektnoOrijentiranoProgramiranje/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektnoOrijentiranoProgramiranje
+{
+    class Program
+    {
+    
+        public class Test
+        {
+            static void Main(string[] args)
+            {
+                sport e = new sport("Nogomet", true, 11);
+                sport r = new sport("Vaterpolo", true);
+                sport t = new sport("Curling");
+            }
+        }
+    }
+}
---
+++ ObjektnoOrijentiranoProgramiranje/ObjektnoOrijentiranoProgramiranje/sport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+   public class sport
+    {
+        private string Naziv ;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+        public sport(string a) { Naziv = a; }
+        public sport(string a, bool b) { Naziv = a; IgraLiSeLoptom = b; }
+        public sport(string a, bool b, byte c) { Naziv = a; IgraLiSeLoptom = b; BrojIgraca = c; }
+    }
+   
+        
+            
+        
+    
+
+
